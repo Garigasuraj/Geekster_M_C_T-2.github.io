e375bb5343d08aa72eb6069f7165aa5524253f0f
Garigasuraj <garigasuraj2127@gmail.com>
2023-02-25 12:44:57 +0530

    git_change

--- javascript.js
+++ javascript.js
@@ -31,16 +31,16 @@ function init(){
             api_call(side_nav_list[i].innerHTML)
         })
     }
-    api_call()
+    api_call()  
 }
 async function api_call(data){
     if(data!=null){
-        const get_data = await fetch(`https://newsapi.org/v2/everything?q=${data}&from=2023-02-09&too=2023-02-09&sortBy=popularity&apiKey=95c60c7edab840ecb918de6fd099d0b9`)
+        const get_data = await fetch(`https://www.newsapi.ai/api/v1/article/getArticles?query=%7B%22%24query%22%3A%7B%22%24and%22%3A%5B%7B%22conceptUri%22%3A%22http%3A%2F%2Fen.wikipedia.org%2Fwiki%2F${data}%22%7D%2C%7B%22lang%22%3A%22eng%22%7D%5D%7D%2C%22%24filter%22%3A%7B%22forceMaxDataTimeWindow%22%3A%2231%22%7D%7D&resultType=articles&articlesSortBy=date&articlesCount=100&articleBodyLen=-1&apiKey=a6dfd44e-ea73-4f19-a4f9-d2a03e44b832`)
         const text_data = await get_data.text()
         const res_string = JSON.parse(text_data)
         rendering_data(res_string)
     }else{
-        const get_data = await fetch(`https://newsapi.org/v2/everything?q=general&from=2023-02-09&too=2023-02-09&sortBy=popularity&apiKey=95c60c7edab840ecb918de6fd099d0b9`)
+        const get_data = await fetch(`https://www.newsapi.ai/api/v1/article/getArticles?query=%7B%22%24query%22%3A%7B%22%24and%22%3A%5B%7B%22conceptUri%22%3A%22http%3A%2F%2Fen.wikipedia.org%2Fwiki%2FNews_media%22%7D%2C%7B%22lang%22%3A%22eng%22%7D%5D%7D%2C%22%24filter%22%3A%7B%22forceMaxDataTimeWindow%22%3A%2231%22%7D%7D&resultType=articles&articlesSortBy=date&articlesCount=100&articleBodyLen=-1&apiKey=a6dfd44e-ea73-4f19-a4f9-d2a03e44b832`)
         const text_data = await get_data.text()
         const res_string = JSON.parse(text_data)
         rendering_data(res_string)
@@ -48,26 +48,26 @@ async function api_call(data){
 }
 function rendering_data(api_data){
     let html = ""
-    for(let i=0; i<50; i++){
+    for(let i=0; i<api_data.articles.results.length; i++){
         html += `
         <div id="content">
            <div id="image_div">
-            <img style="width: 275px; height = auto" src="${api_data.articles[i].urlToImage}">
+            <img style="width: 275px; height = auto" src="${api_data.articles.results[i].image}">
         </div>
         <div id="content_div" style="margin: 8px 10px;">
-            <div id="heading">${api_data.articles[i].title}</div>  
+            <div id="heading">${api_data.articles.results[i].title}</div>  
 
             <div id="author_time">
-                <span style="font-weight: bold; color: grey;"><a id="link_1" href="${api_data.articles[i].url}">short</a></span>
+                <span style="font-weight: bold; color: grey;"><a id="link_1" href="${api_data.articles.results[i].url}">short</a></span>
                 
-                <span style="color: rgb(149, 146, 146);"> ${api_data.articles[i].author} / ${api_data.articles[i].publishedAt}</span>
+                <span style="color: rgb(149, 146, 146);"> ${api_data.articles.results[i].source.title}/ ${api_data.articles.results[i].date}${api_data.articles.results[i].time}</span>
             </div>
 
-            <div id="description">${api_data.articles[i].description}</div>
+            <div id="description">${api_data.articles.results[i].body.slice(0,400)}...</div>
 
             <div id="new_info_link">
             <span>Readmore at<span> 
-            <span style="font-weight: bold;"><a id="link" href="${api_data.articles[i].url}">${api_data.articles[i].source.name}</a></span></div>
+            <span style="font-weight: bold;"><a id="link" href="${api_data.articles.results[i].url}">${api_data.articles.results[i].source.uri}</a></span></div>
             </div>
         </div>`
     }
